c50bcb3d05aa107682e0a9b8175348d695f65908
Matheus Mendes <mattheus2015@yahoo.com.br>
2021-03-04 15:10:07 -0300

    bugfix: profile fields needs to be nullable

--- registry/models.py
+++ registry/models.py
@@ -14,20 +14,28 @@ class Profile(models.Model):
         on_delete=models.CASCADE
     )
 
-    country = models.CharField(_('country'), max_length=CHAR_MAX_LENGTH)
-    state = models.CharField(_('state'), max_length=CHAR_MAX_LENGTH)
-    city = models.CharField(_('city'), max_length=CHAR_MAX_LENGTH)
-    cep = models.IntegerField(_('cep'))
-    street = models.CharField(_('street'), max_length=CHAR_MAX_LENGTH)
-    houseNumber = models.PositiveIntegerField(_('houseNumber'))
+    country = models.CharField(
+        _('country'),
+        max_length=CHAR_MAX_LENGTH,
+        null=True
+    )
+    state = models.CharField(_('state'), max_length=CHAR_MAX_LENGTH, null=True)
+    city = models.CharField(_('city'), max_length=CHAR_MAX_LENGTH, null=True)
+    cep = models.IntegerField(_('cep'), null=True)
+    street = models.CharField(
+        _('street'),
+        max_length=CHAR_MAX_LENGTH,
+        null=True
+    )
+    houseNumber = models.PositiveIntegerField(_('houseNumber'), null=True)
     complement = models.CharField(
         _('complement'),
         max_length=CHAR_MAX_LENGTH,
         null=True
     )
 
-    cpf = models.IntegerField(_('cpf'))
-    pis = models.IntegerField(_('pis'))
+    cpf = models.IntegerField(_('cpf'), null=True)
+    pis = models.IntegerField(_('pis'), null=True)
 
 
 @receiver(post_save, sender=User)
