557d13a469b97beaf08f53e23ccc282d4b4d6fca
Matheus Mendes <mattheus2015@yahoo.com.br>
2021-03-05 21:16:59 -0300

    feat: fixed mtm create error

--- registry/serializers.py
+++ registry/serializers.py
@@ -1,5 +1,6 @@
 from django.db import models
 from rest_framework.serializers import ModelSerializer
+from django.contrib.auth.hashers import make_password
 from .models import Profile, User
 
 
@@ -19,3 +20,23 @@ class UserSerializer(ModelSerializer):
             'last_name', 'email',
             'profile', 'password'
         ]
+
+    def create(self, validatedData):
+        username = validatedData.get('first_name').lower()
+        if validatedData.get('last_name', None) is not None:
+            username += validatedData.get('last_name')
+        user = User.objects.create(
+            username=username,
+            first_name=validatedData.get('first_name'),
+            last_name=validatedData.get('last_name'),
+            email=validatedData.get('email'),
+            password=make_password(validatedData.get('password'))
+        )
+
+        defaults = validatedData['profile']
+        defaults['user'] = user
+        profile = Profile.objects.update_or_create(
+            user=user,
+            defaults=defaults
+        )
+        return user
