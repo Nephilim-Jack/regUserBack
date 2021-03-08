230ddf38fa28433d2372f0a8479baf3ac3e902f2
Matheus Mendes <mattheus2015@yahoo.com.br>
2021-03-08 09:43:31 -0300

    feat: jwt auth added

--- regUser/settings.py
+++ regUser/settings.py
@@ -101,6 +101,15 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
--- regUser/urls.py
+++ regUser/urls.py
@@ -16,14 +16,24 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView
+)
 
 
 def moveToMain(request):
-    return redirect('api/v1/users/')
+    return redirect('api/v1/')
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(
+        'api/token/login/',
+        TokenObtainPairView.as_view(),
+        name='token_obtain_pair'
+    ),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/v1/', include('registry.urls')),
     path('', moveToMain)
 ]
